5ee7794af44804cf9e504824415ed2339387a928
Tyuiu.KoptyaevRS@outlook.com <rustam.koptyaev@mail.ru>
2023-10-21 19:39:48 +0500

    Task3

---
+++ Tyuiu.KoptyaevRS.Sprint3.Task3.V26.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KoptyaevRS.Sprint3.Task3.V26.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KoptyaevRS.Sprint3.Task3.V26.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f9f3d2f2-e825-4481-86c5-7b975f4f1a6e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KoptyaevRS.Sprint3.Task3.V26/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KoptyaevRS.Sprint3.Task3.V26.Lib;
+
+namespace Tyuiu.KoptyaevRS.Sprint3.Task3.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Коптяев Р. С. | АСОиУБ-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Коптяев Рустам Сергеевич | АСОиУБ-23-3                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach подсчитать количество букв “e” в строке:         *");
+            Console.WriteLine("* have a nice time                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "have a nice time";
+            char chr = 'e';
+
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Исходный символ = " + chr);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество символов = " + ds.GetCharCount(value, chr));
+            Console.ReadKey();
+        }
+    }
+}
