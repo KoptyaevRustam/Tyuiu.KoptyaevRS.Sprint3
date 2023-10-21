ec889a5a1e1401d00a1837e444df5c28030c9959
Tyuiu.KoptyaevRS@outlook.com <rustam.koptyaev@mail.ru>
2023-10-21 19:14:53 +0500

    Task2

---
+++ Tyuiu.KoptyaevRS.Sprint3.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KoptyaevRS.Sprint3.Task2.V23.Lib;
+
+namespace Tyuiu.KoptyaevRS.Sprint3.Task2.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+
+            double value = 1.5;
+            int startValue = 1;
+            int stopValue = 13;
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+
+            double wait = -63.727;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
